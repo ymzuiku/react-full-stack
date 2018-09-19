7a31222fabe8185c2ef6696e4edc118fff0ef1fa
pillar <pillar@appledeMacBook-Pro.local>
2018-09-19 23:08:55 +0800

    save

--- shipitfile.js
+++ shipitfile.js
@@ -2,7 +2,6 @@ module.exports = shipit => {
   require('shipit-deploy')(shipit);
   const appName = 'full-stack';
   const remotePath = `/db/ssr/${appName}`;
-  const currentPath = remotePath + '/current';
   shipit.initConfig({
     default: {
       deployTo: remotePath,
@@ -13,22 +12,22 @@ module.exports = shipit => {
       branch: 'master',
     },
   });
-  shipit.task('copy', async () => {
+  shipit.task('copyToRemote', async () => {
     await shipit.copyToRemote('./ssh', '/db/static/public/gantt');
   });
   shipit.task('prod', async () => {
-    await shipit.remote(`pm2 delete ${appName}-server || echo`);
+    await shipit.remote(`pm2 delete ${appName}-server || echo ''`);
     await shipit.remote(
-      `cd ${currentPath} && yarn && cd server && port=4000 deploy=1 pm2 start server.js --name=${appName}-server -i 0 || echo`,
+      `cd ${remotePath}/current && yarn install && cd server && port=4000 deploy=1 pm2 start server.js --name=$s{appName}-server -i 0 || echo ''`,
     );
 
-    await shipit.remote(`pm2 delete ${appName}-client || echo`);
+    await shipit.remote(`pm2 delete ${appName}-client || echo ''`);
     await shipit.remote(
-      `cd ${currentPath}/client && ../node_modules/.bin/next build && port=80 deploy=1 pm2 start client.js --name=${appName}-client -i 0 || echo`,
+      `cd ${remotePath}/current/client && ../node_modules/.bin/next build && port=80 deploy=1 pm2 start client.js --name=${appName}-client -i 0 || echo ''`,
     );
   });
   // listen published event
   shipit.on('published', () => {
-    shipit.start(['copy', 'prod']);
+    shipit.start(['copyToRemote', 'prod']);
   });
 };
