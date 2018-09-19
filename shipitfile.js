430cb76e4e26514950335d6d645e17f60cf71019
pillar <pillar@appledeMacBook-Pro.local>
2018-09-19 22:27:35 +0800

    save

--- shipitfile.js
+++ shipitfile.js
@@ -2,7 +2,6 @@ module.exports = shipit => {
   require('shipit-deploy')(shipit);
   const appName = 'full-stack';
   const remotePath = `/db/ssr/${appName}`;
-  const currentPath = remotePath + '/current';
   shipit.initConfig({
     default: {
       deployTo: remotePath,
@@ -19,12 +18,12 @@ module.exports = shipit => {
   shipit.task('prod', async () => {
     await shipit.remote(`pm2 delete ${appName}-server || echo ''`);
     await shipit.remote(
-      `cd ${currentPath} && yarn && cd server && port=4000 deploy=1 pm2 start server.js --name=${appName}-server -i 0 || echo ''`,
+      `cd ${remotePath}/current && yarn && cd server && port=4000 deploy=1 pm2 start server.js --name=${appName}-server -i 0 || echo ''`,
     );
 
     await shipit.remote(`pm2 delete ${appName}-client || echo ''`);
     await shipit.remote(
-      `cd ${currentPath}/client && ../node_modules/.bin/next build && port=80 deploy=1 pm2 start client.js --name=${appName}-client -i 0 || echo ''`,
+      `cd ${remotePath}/current/client && ../node_modules/.bin/next build && port=80 deploy=1 pm2 start client.js --name=${appName}-client -i 0 || echo ''`,
     );
   });
   // listen published event
